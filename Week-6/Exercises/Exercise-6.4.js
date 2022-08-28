4a17673eb351ecf9b6cbd3a541045a2744494f6c
AravindSirivelli <aravindsirivelli97@gmail.com>
2022-08-28 11:10:35 +0530

    fixed maxProfit

--- Week-6/Exercises/Exercise-6.4.js
+++ Week-6/Exercises/Exercise-6.4.js
@@ -6,20 +6,17 @@
 //Example 1: Input: prices = [7,1,5,3,6,4] Output: 5 Explanation: Buy on day 2 (price = 1)and sell on day 5 (price = 6), profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell. 
 //Example 2: Input: prices =[7,6,4,3,1] Output: 0 Explanation: In this case, no transactions are done and the maxprofit = 0 Constraints: 1 <= prices.length <= 105 0 <= prices[i] <= 104
 
-const maxProfit = (prices) => {
-    let bestprofit = 0;
-    if (prices.length === 0) return bestprofit;
-    const bestBuyValue = Math.min(...prices);
-    const bestBuyDay = prices.indexOf(bestBuyValue);
-    for (let i = bestBuyDay; i < prices.length; i++) {
-        let currentprofit = prices[i] - bestBuyValue;
-        if (bestprofit < currentprofit) {
-            bestprofit = currentprofit;
-        }
+var maxProfit = function (prices) {
+    let globalProfit = 0;
+    let minBuyPrice = Infinity;
+    for (let i = 0; i < prices.length; i++) {
+      if (minBuyPrice > prices[i]) {
+        minBuyPrice = prices[i];
+      }
+      globalProfit = Math.max(globalProfit,prices[i] - minBuyPrice);
     }
-    return bestprofit;
-}
-
+    return globalProfit;
+  };
 
 maxProfit([11, 2, 20, 6, 5]);  //result = 18
 
